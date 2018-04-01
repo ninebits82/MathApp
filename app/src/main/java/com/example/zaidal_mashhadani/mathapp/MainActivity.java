6d12c3d058cd18b8a6947595622374770a5d287c
cpena18 <ninebits82@gmail.com>
2018-04-01 01:42:10 -0400

    adding a reset button for new attempt

--- app/src/main/java/com/example/zaidal_mashhadani/mathapp/MainActivity.java
+++ app/src/main/java/com/example/zaidal_mashhadani/mathapp/MainActivity.java
@@ -7,6 +7,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
 
@@ -16,21 +18,44 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button but1 = (Button)findViewById(R.id.result);
+        Button reset = (Button) findViewById(R.id.reset);
         final EditText num1 = (EditText) findViewById(R.id.in1);
         final EditText num2 = (EditText) findViewById(R.id.in2);
         final EditText result = (EditText) findViewById(R.id.userAns);
+
+        /**
+        * generate random numbers
+        * */
+
+        final Random r = new Random();
+        final Random r1 = new Random();
+        final int rn1 = r.nextInt(10 - 1) + 1;
+        final int rn2 = r1.nextInt(10 - 1) + 1;
+
+        /**
+         * display these random numbers in the text fields
+         */
+
+
+        num1.setText(Integer.toString(rn1));
+        num2.setText(Integer.toString(rn2));
+
+
+
         but1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String str1 = num1.getText().toString();
-                String str2 = num2.getText().toString();
-                String str3 = result.getText().toString();
                 int n1 = Integer.parseInt(str1);
-                int n2 = Integer.parseInt(str2);
-                int n3 = Integer.parseInt(str3);
-                //result.setText(Integer.toString(n1 * n2));
 
-               if(n3 == n1 * n2){
+                String str2 = num2.getText().toString();
+                int n2 = Integer.parseInt(str2);
+
+
+                String str3 = result.getText().toString();
+                int n0 = Integer.parseInt(str3);
+
+               if(n0 == n1 * n2){
                    Toast.makeText(getApplicationContext(),"Good Job!", Toast.LENGTH_SHORT)
                            .show();
 
@@ -40,6 +65,19 @@ public class MainActivity extends AppCompatActivity {
                            .show();
                }
             }
+        });
+        reset.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                final Random r = new Random();
+                final Random r1 = new Random();
+                final int rn1 = r.nextInt(10 - 1) + 1;
+                final int rn2 = r1.nextInt(10 - 1) + 1;
+
+                num1.setText(Integer.toString(rn1));
+                num2.setText(Integer.toString(rn2));
+            }
+
         });
     }
 
